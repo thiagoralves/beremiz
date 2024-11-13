1d5456a58c1c22b579ec439db04709559d08f9b1
John Jamieson <jjamieson007@gmail.com>
2024-11-13 20:46:41 +1300

    Rewrite of rp2040pico to fix PWM issues

--- arduino/src/hal/rp2040pico.cpp
+++ arduino/src/hal/rp2040pico.cpp
@@ -4,6 +4,7 @@ extern "C" {
 }
 #include "Arduino.h"
 #include "../examples/Baremetal/defines.h"
+#include "RP2040_PWM.h"
 
 //OpenPLC HAL for Raspberry Pi Pico/Pico W with the RP2040
 
@@ -14,25 +15,59 @@ Analog In: A1, A2, A3 (26,27,28)             (%IW0 - %IW2)
 Analog Out: 4,5                              (%QW0 - %QW1)
 **************************************************************/
 
-/*
-    Refer to Pico-OpenPLC-A4-Pinout.pdf & -PicoPLC schem.pdf for details
-    on using the Pico/Pico W as a plc
-    An additional 2 analogue outputs can be used with GPIO2 & 3 if the      
-    SPI functionality is not required (adding extra code to the .ino file)     
-*/
+// Arduino wiring - https://arduino-pico.readthedocs.io/en/latest/analog.html
+// RP2040_PWM - https://github.com/khoih-prog/RP2040_PWM
+// PWM settings
+#define PWM_FREQ 10000.0f
+#define PWM_BITS 16
+#define PWM_RANGE 65535 // can be from 16-65535
+RP2040_PWM *PWM[NUM_ANALOG_OUTPUT]; 
+bool PWM_block[NUM_ANALOG_OUTPUT]; //track which pins use the PWM_CONTROLLER block
+
+//this call is required for the C-based PWM block on the Editor
+extern "C" uint8_t set_hardware_pwm(uint8_t, float, float);
+
 //Create the I/O pin masks
 uint8_t pinMask_DIN[] = {PINMASK_DIN};
 uint8_t pinMask_AIN[] = {PINMASK_AIN};
 uint8_t pinMask_DOUT[] = {PINMASK_DOUT};
 uint8_t pinMask_AOUT[] = {PINMASK_AOUT}; //2,3 can be used if SPI not required
 
+float pwm_freq = PWM_FREQ;
+bool toggle = true;
+
+// called by the PWM_CONTROLLER
+uint8_t set_hardware_pwm(uint8_t ch, float freq, float duty)
+{
+    if (ch >= NUM_ANALOG_OUTPUT) return 0;
+
+    //range check 
+    duty = duty < 0 ? 0 : duty > 100 ? 100 : duty;
+
+    //using the RP2040_PWM library
+    if (PWM[ch]->setPWM_Int(pinMask_AOUT[ch],freq, duty * 1000))
+    {   
+        PWM_block[ch] = true; //this channel is using the block
+        pwm_freq = freq;
+        return 1;
+    }
+
+    return 0;
+}
+
 void hardwareInit()
 {
     // set the analog input resolution to 12bit
     analogReadResolution(12);
-    // set the analog output resolution to 16bit
-    analogWriteResolution(16);
-    
+    pinMode(25, OUTPUT);
+
+    //Arduino wiring library without using the defaults
+    /*
+    analogWriteFreq(PWM_FREQ); 
+    analogWriteRange(PWM_RANGE); 
+    analogWriteResolution(PWM_BITS);
+    //*/
+
     for (int i = 0; i < NUM_DISCRETE_INPUT; i++)
     {
         pinMode(pinMask_DIN[i], INPUT);
@@ -47,7 +82,13 @@ void hardwareInit()
     }
     for (int i = 0; i < NUM_ANALOG_OUTPUT; i++)
     {
-        pinMode(pinMask_AOUT[i], OUTPUT);
+        //using the RP2040_PWM library
+        PWM_block[i] = false; //assume no blocks used
+        PWM[i] = new RP2040_PWM(pinMask_AOUT[i], PWM_FREQ, 0); 
+        if (PWM[i] )  PWM[i] ->setPWM();
+    
+        //Arduino wiring library
+        //pinMode(pinMask_AOUT[i], OUTPUT);
     }
 }
 
@@ -61,26 +102,42 @@ void updateInputBuffers()
     for (int i = 0; i < NUM_ANALOG_INPUT; i++)
     {
         if (int_input[i] != NULL)
-        /*
-            Changed to work with the Raspberry Pi Pico/Pico W
-            Nano connect uses the A0-A3 from the RP2040
-            and A4-A7 from the Nina-W102
-        */		
-//            *int_input[i] = (analogRead(pinMask_AIN[i]) * 64);
-            *int_input[i] = analogRead(pinMask_AIN[i]);
+          *int_input[i] = analogRead(pinMask_AIN[i]);
     }
 }
 
 void updateOutputBuffers()
 {
+    uint32_t duty = 0;    
+   
+
     for (int i = 0; i < NUM_DISCRETE_OUTPUT; i++)
     {
         if (bool_output[i/8][i%8] != NULL) 
             digitalWrite(pinMask_DOUT[i], *bool_output[i/8][i%8]);
     }
+
     for (int i = 0; i < NUM_ANALOG_OUTPUT; i++)
     {
+        int _duty = 0;
         if (int_output[i] != NULL) 
-            analogWrite(pinMask_AOUT[i], (*int_output[i] / 256));
+        {
+            // The analogWrite line assumes arduino defaults without library without
+            // analogWriteFreq, analogWriteRange, analogWriteResolution
+            //analogWrite(pinMask_AOUT[i], (*int_output[i])/256);
+            
+            _duty = *int_output[i];
+            _duty = _duty < 0 ? 0 : _duty > PWM_RANGE ? PWM_RANGE : _duty;
+
+            //using the RP2040_PWM library
+            if (PWM_block[i] == false) //only pins not using the PWM_CONTROLLER block
+            {  
+               duty = map(_duty,0,65535,0,100);             
+               PWM[i]->setPWM_Int(pinMask_AOUT[i], pwm_freq, duty*1000);
+            }
+
+            //Arduino wiring library
+            //analogWrite(pinMask_AOUT[i], _duty);
+        }  
     }
 }
